c259c73c3add0f0da2408de708bd845bd56d750a
ky0shir089 <ky0shir0_13@yahoo.co.id>
2025-08-14 17:02:51 +0700

    fix prisma

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextConfig } from "next";
+//@ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,6 +13,13 @@ const nextConfig: NextConfig = {
         port: "",
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
 
